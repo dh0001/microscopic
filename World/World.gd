6afa3563c38702d7359a31c4dca7a943d49a79b3
David Hu <d26hu@uwaterloo.ca>
2023-06-09 19:02:57 -0400

    add shrink expand

--- World/World.gd
+++ World/World.gd
@@ -5,6 +5,7 @@ extends Node2D
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		swap_fullscreen_mode()
+		
 
 func swap_fullscreen_mode():
 	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
